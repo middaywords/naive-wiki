d49498c849307fa7381e9c32a4cf209fe48848a3
YiXR <37775155+YiXR@users.noreply.github.com>
2021-06-13 16:44:09 +0800

    class autocorrectWC  .correct_term() .wild_card()

--- utils/wild_card.py
+++ utils/wild_card.py
@@ -2,87 +2,89 @@ from autocorrect import Speller
 import os
 import json
 
+class autocorrectWC():
+    def __init__(self):
+        with open(f"../data/permutermIndex.json", 'r') as loadFile:
+            permuterm = json.load(loadFile)
+        self.permuterm = permuterm
 
-def correct_term(term: str) -> str:
-    # Autocorrect
-    correcter = Speller()
-
-    return correcter.autocorrect_word(term)
-
-
-def prefix_match(term, prefix):
-    term_list = []
-    for tk in term[prefix[0]].keys():
-        if tk.startswith(prefix):
-            term_list.append(term[prefix[0]][tk])
-    return term_list
-
-
-def prefix_matchA(term, prefix):
-    term_list = {}
-    for tk in term[prefix[0]].keys():
-        if tk.startswith(prefix):
-            term_list[term[prefix[0]][tk]] = 1
-    return term_list
-
-
-def prefix_matchB(term, prefix, termA):
-    term_list = []
-    for tk in term[prefix[0]].keys():
-        if tk.startswith(prefix):
-            if term[prefix[0]][tk] in termA:
+    def prefix_match(self,term, prefix):
+        term_list = []
+        for tk in term[prefix[0]].keys():
+            if tk.startswith(prefix):
                 term_list.append(term[prefix[0]][tk])
-    return term_list
+        return term_list
 
 
-def wild_card(term: str):
-    """
-    query: lower case str
-    """
-    parts = term.split("*")
+    def prefix_matchA(self,term, prefix):
+        term_list = {}
+        for tk in term[prefix[0]].keys():
+            if tk.startswith(prefix):
+                term_list[term[prefix[0]][tk]] = 1
+        return term_list
 
-    if len(parts) == 3:
-        case = 4
-    elif parts[1] == '':
-        case = 1
-    elif parts[0] == '':
-        case = 2
-    elif parts[0] != '' and parts[1] != '':
-        case = 3
 
-    if case == 4:
-        if parts[0] == '':
+    def prefix_matchB(self,term, prefix, termA):
+        term_list = []
+        for tk in term[prefix[0]].keys():
+            if tk.startswith(prefix):
+                if term[prefix[0]][tk] in termA:
+                    term_list.append(term[prefix[0]][tk])
+        return term_list
+
+
+    def wild_card(self, term: str):
+        """
+        query: lower case str
+        """
+        parts = term.split("*")
+
+        if len(parts) == 3:
+            case = 4
+        elif parts[1] == '':
             case = 1
+        elif parts[0] == '':
+            case = 2
+        elif parts[0] != '' and parts[1] != '':
+            case = 3
 
-    if case == 1:
-        query = parts[0]
-    elif case == 2:
-        query = parts[1] + "$"
-    elif case == 3:
-        query = parts[1] + "$" + parts[0]
-    elif case == 4:
-        queryA = parts[2] + "$" + parts[0]
-        queryB = parts[1]
+        if case == 4:
+            if parts[0] == '':
+                case = 1
 
-    with open(f"../data/permutermIndex.json", 'r') as loadFile:
-        permuterm = json.load(loadFile)
+        if case == 1:
+            query = parts[0]
+        elif case == 2:
+            query = parts[1] + "$"
+        elif case == 3:
+            query = parts[1] + "$" + parts[0]
+        elif case == 4:
+            queryA = parts[2] + "$" + parts[0]
+            queryB = parts[1]
 
-    print('start')
 
-    if case != 4:
-        term_list = prefix_match(permuterm, query)
+        if case != 4:
+            term_list = self.prefix_match(self.permuterm, query)
 
-    elif case == 4:
-        # queryA Z$X
-        term_listA = prefix_matchA(permuterm, queryA)
+        elif case == 4:
+            # queryA Z$X
+            term_listA = self.prefix_matchA(self.permuterm, queryA)
 
-        # queryB Y
-        term_list = prefix_matchB(permuterm, queryB, term_listA)
+            # queryB Y
+            term_list = self.prefix_matchB(self.permuterm, queryB, term_listA)
 
-    return term_list
+        return term_list
+
+    def correct_term(self,term: str) -> str:
+        # Autocorrect
+        correcter = Speller()
+
+        return correcter.autocorrect_word(term)
 
 
 if __name__ == '__main__':
     inputSentence = 'h*l*o'
-    terms = wild_card(inputSentence)
+    wc = autocorrectWC()
+    
+    terms = wc.correct_term(inputSentence)
     print(terms)
